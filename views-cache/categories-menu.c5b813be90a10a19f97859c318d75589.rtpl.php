db7f235b099d5641ee8c58daf070e9eabd239cc2
WilliamFontesOliveira <52585999+WilliamFontesOliveira@users.noreply.github.com>
2020-11-10 11:33:49 -0300

    Add files via upload

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Adesivos</a></li><li><a href="/categories/3">Convites</a></li><li><a href="/categories/5">Envelopes</a></li><li><a href="/categories/9">Kits para festa</a></li><li><a href="/categories/4">Lembrancinhas</a></li><li><a href="/categories/7">Tags</a></li><li><a href="/categories/8">Topos de Bolo</a></li>
