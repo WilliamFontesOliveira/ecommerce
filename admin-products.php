2173408251cc2ef9fc00162548b4db34c4e9227c
WilliamFontesOliveira <52585999+WilliamFontesOliveira@users.noreply.github.com>
2020-12-02 11:27:27 -0300

    Add files via upload

--- admin-products.php
+++ admin-products.php
@@ -2,7 +2,7 @@
 
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
-use \Hcode\Product;
+use \Hcode\Model\Product;
 
 $app->get("/admin/products", function(){
 
@@ -16,6 +16,30 @@ $app->get("/admin/products", function(){
 		"products"=>$products
 	]);
 });
+
+$app->get("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-create");
+});
+
+$app->post("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->setData($_POST);
+
+	$product->save();
+
+	header("Location: /admin/products");
+	exit;
+});
+
 
 
 ?>
