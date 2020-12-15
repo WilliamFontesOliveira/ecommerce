e64517389d836619b72e3d8c651d256ab47844af
WilliamFontesOliveira <52585999+WilliamFontesOliveira@users.noreply.github.com>
2020-12-14 21:23:19 -0300

    Add files via upload

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -17,24 +17,24 @@ class Product extends Model {
 	}
 
 	public function save()
-	{
-
-		$sql = new Sql();
-
-		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight :desurl)", 
-			array(
-			":idproduct"=>$this->getidproduct(),
-			":desproduct"=>$this->getdesproduct(),
-			":vlprice"=>$this->getvlprice(),
-			":vlwidth"=>$this->getvlwidth(),
-			":vlheight"=>$this->getvlheight(),
-			":vllength"=>$this->getvllength(),
-			":vlweigth"=>$this->getvlweigth(),
-			":desurl"=>$this->getdesurl()
-		));
-
-		$this->setData($results[0]);
-	}
+    {
+ 
+        $sql = new Sql();
+ 
+        $results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", 
+            array(
+            ":idproduct"=>$this->getidproduct(),
+            ":desproduct"=>$this->getdesproduct(),
+            ":vlprice"=>$this->getvlprice(),
+            ":vlwidth"=>$this->getvlwidth(),
+            ":vlheight"=>$this->getvlheight(),
+            ":vllength"=>$this->getvllength(),
+            ":vlweight"=>$this->getvlweight(),
+            ":desurl"=>$this->getdesurl()
+        ));
+ 
+        $this->setData($results[0]);
+    }
 
 	public function get($idproduct)
 	{
@@ -57,7 +57,75 @@ class Product extends Model {
 				':idproduct'=>$this->getidproduct()
 		]);
 	}
-	
+
+	public function checkPhoto()
+	{
+		if (file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduto() . ".jpg"
+		)) {
+
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
+		} else {
+
+			$url = "/res/site/img/product.jpg";
+		}
+
+		return $this->setdesphoto($url);
+	}
+
+	public function getValues()
+	{
+
+		$this->checkPhoto();
+
+		$values = parent::getValues();
+
+		return $values;
+	}
+
+	public function setPhoto($file)
+	{
+		$extension = explode('.', $file['name']);
+		$extension = end($extension);
+
+		switch ($extension)	{
+
+			case "jpg":
+			case "jpeg":
+			$image = imagecreatefromjpeg($file["tmp_name"]);
+			break;
+
+			case "gif":
+			$image = imagecreatefromgif($file["tmp_name"]);
+
+			break;
+
+			case "png":
+			$image = imagecreatefrompng($file["tmp_name"]);
+
+			break;
+		}
+
+		$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduto() . ".jpg";
+
+		imagejpeg($image, $dist);
+		
+		imagedestroy($image);
+
+		$this->checkPhoto();	
+
+	}
+
+
 }
 
 ?>
